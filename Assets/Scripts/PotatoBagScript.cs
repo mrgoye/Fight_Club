6fee9665659185d7892e59266f608fd3386052b6
VLB <victor.lebail@y-nov.com>
2017-01-18 16:44:29 +0100

    added health bar

--- Assets/Scripts/PotatoBagScript.cs
+++ Assets/Scripts/PotatoBagScript.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class PotatoBagScript : MonoBehaviour {
 
+    PlayerHealthScript phs;
 	public float force;
 
 	// Use this for initialization
 	void Start () {
-	
+        phs = gameObject.GetComponentInChildren<PlayerHealthScript>() ;
 	}
 	
 	// Update is called once per frame
@@ -16,13 +17,15 @@ public class PotatoBagScript : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D obj){
-		if (obj.gameObject.layer == 8) { 
-			GetComponent<Animator> ().SetBool ("isPunched", true);
-			this.transform.Translate (Vector2.up * force);
-			this.transform.Translate(Vector2.right * force);
-			GlobalScript.combo++;
-			Debug.Log(GlobalScript.combo);
-		}
+        if (obj.gameObject.layer == 8)
+        {
+            phs.takeDamage(10);
+            GetComponent<Animator>().SetBool("isPunched", true);
+            this.transform.Translate(Vector2.up * force);
+            this.transform.Translate(Vector2.right * force);
+            GlobalScript.combo++;
+            Debug.Log(GlobalScript.combo);
+        }
 	}
 
 	void OnTriggerExit2D(Collider2D obj) {
